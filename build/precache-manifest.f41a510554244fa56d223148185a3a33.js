ed88f22098db92c45a850a123680ecf2355af59f
SHIKHAace <81000425+SHIKHAace@users.noreply.github.com>
2021-03-20 09:47:33 +0530

    build folder

---
+++ build/precache-manifest.f41a510554244fa56d223148185a3a33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89afc304e30c7b515a4f0c17657c57e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afb7cea9e954984419a6",
+    "url": "/static/js/2.9f66c83a.chunk.js"
+  },
+  {
+    "revision": "4d1cdf6dc1eb219ca7ea700e96e657f5",
+    "url": "/static/js/2.9f66c83a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b800b3fb2d167dc3d00",
+    "url": "/static/js/main.020364f1.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
